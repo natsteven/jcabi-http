26765f546e6d2a2b71e6a71ff755a22ec571c2f0
natsteven <nathanaelsteven@gmail.com>
2025-04-27 13:31:59 -0600

    method coverage

--- src/test/java/com/jcabi/http/CustomRegressionTests.java
+++ src/test/java/com/jcabi/http/CustomRegressionTests.java
@@ -1,191 +1,191 @@
-//package com.jcabi.http;
-//
-//import com.jcabi.http.request.*;
-//import com.jcabi.http.response.*;
-//import com.jcabi.http.wire.*;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mockito;
-//import com.jcabi.immutable.Array;
-//import java.util.AbstractMap;
-//import java.net.URL;
-//import java.util.Map;
-//
-//public class CustomRegressionTests {
-//
-//    // TRIVIAL TESTS: ---------------------------------------
-//    // This tests the toString, equals, and hashCode methods of all classes.
-//    // These are often generated through the lombok package and aren't really related to functionality.
-//    // Certain other trivial coverage of, for example, unused constructors is also achieved here.
-//    @Test
-//    void toStringTests() throws Exception {
-//        try {
-//            // Requests Classes
-//            String urlString = "http://localhost:8080/";
-//            URL basicURL = new URL(urlString);
-//            Wire wire = Mockito.mock(Wire.class);
-//
-//            ApacheRequest apacheRequest = new ApacheRequest(basicURL);
-//            String expected = "HTTP/1.1 GET / (localhost)\n\n";
-//            assert(apacheRequest.toString().contains(expected));
-//
-//            BaseRequest baseRequest = new BaseRequest(wire, urlString);
-//            assert(baseRequest.toString().contains(expected));
-//            assert(baseRequest.uri().toString().equals(urlString));
-//            RequestBody body = baseRequest.body().formParam("a","b").formParam("c","d");
-//            assert(body.toString().equals("a=b&c=d"));
-//            assert(baseRequest.multipartBody().toString().contains("<<empty>>"));
-//
-//            FakeRequest fakeRequest = new FakeRequest();
-//            assert(fakeRequest.toString().contains("FakeRequest"));
-//
-//            JdkRequest jdkRequest = new JdkRequest(urlString);
-//            assert(jdkRequest.toString().contains("JdkRequest"));
-//
-//            // Wire Classes
-//            AutoRedirectingWire wire1 = new AutoRedirectingWire(wire);
-//            assert(wire1.toString().contains("AutoRedirectingWire"));
-//
-//            BasicAuthWire wire2 = new BasicAuthWire(wire);
-//            assert(wire2.toString().contains("BasicAuthWire"));
-//
-//            CachingWire wire3 = new CachingWire(wire);
-//            assert(wire3.toString().contains("CachingWire"));
-//
-//            CookieOptimizingWire wire4 = new CookieOptimizingWire(wire);
-//            assert(wire4.toString().contains("CookieOptimizingWire"));
-//
-//            ETagCachingWire wire5 = new ETagCachingWire(wire);
-//            assert(wire5.toString().contains("ETagCachingWire"));
-//
-//            FcWire wire6 = new FcWire(wire);
-//            assert(wire6.toString().contains("FcWire"));
-//
-//            LastModifiedCachingWire wire7 = new LastModifiedCachingWire(wire);
-//            assert(wire7.toString().contains("LastModifiedCachingWire"));
-//
-//            OneMinuteWire wire8 = new OneMinuteWire(wire);
-//            assert(wire8.toString().contains("OneMinuteWire"));
-//
-//            RetryWire wire9 = new RetryWire(wire);
-//            assert(wire9.toString().contains("RetryWire"));
-//
-//            TrustedWire wire10 = new TrustedWire(wire);
-//            assert(wire10.toString().contains("TrustedWire"));
-//
-//            UserAgentWire wire11 = new UserAgentWire(wire);
-//            assert(wire11.toString().contains("UserAgentWire"));
-//
-//            VerboseWire wire12 = new VerboseWire(wire);
-//            assert(wire12.toString().contains("VerboseWire"));
-//
-//        } catch (Exception e) {
-//            System.out.println("Exception in toStringTests: " + e.getMessage());
-//        }
-//    }
-//
-//    @Test
-//    void equalsTest() throws Exception {
-//        try {
-//            String urlString = "http://localhost:8080/";
-//            URL basicURL = new URL(urlString);
-//            Wire wire = Mockito.mock(Wire.class);
-//
-//            // Create identical headers and content
-//            Array<Map.Entry<String, String>> headers = new Array<>(
-//                    new AbstractMap.SimpleEntry<>("Content-Type", "application/json"),
-//                    new AbstractMap.SimpleEntry<>("Authorization", "Bearer token")
-//            );
-//            byte[] content = "response body".getBytes();
-//
-//            ApacheRequest apacheRequest = new ApacheRequest(basicURL);
-//            ApacheRequest apacheRequest2 = new ApacheRequest(basicURL);
-//            assert (apacheRequest.equals(apacheRequest2));
-//
-//            DefaultResponse defaultResponse = new DefaultResponse(apacheRequest, 200, "OK", headers, content);
-//            DefaultResponse defaultResponse2 = new DefaultResponse(apacheRequest2, 200, "OK", headers, content);
-//            assert (defaultResponse.equals(defaultResponse2));
-//
-//            FakeRequest fakeRequest = new FakeRequest();
-//            FakeRequest fakeRequest2 = new FakeRequest();
-//            assert (fakeRequest.equals(fakeRequest2));
-//
-//            JdkRequest jdkRequest = new JdkRequest(urlString);
-//            JdkRequest jdkRequest2 = new JdkRequest(urlString);
-//            assert (jdkRequest.equals(jdkRequest2));
-//
-//            // Response Classes
-//            Response response = Mockito.mock(Response.class);
-//
-//            JacksonResponse jacksonResponse = new JacksonResponse(response);
-//            JacksonResponse jacksonResponse2 = new JacksonResponse(response);
-//            assert (jacksonResponse.equals(jacksonResponse2));
-//
-//            JsonResponse jsonResponse = new JsonResponse(response);
-//            JsonResponse jsonResponse2 = new JsonResponse(response);
-//            assert (jsonResponse.equals(jsonResponse2));
-//
-//            JsoupResponse jsoupResponse = new JsoupResponse(response);
-//            JsoupResponse jsoupResponse2 = new JsoupResponse(response);
-//            assert (jsoupResponse.equals(jsoupResponse2));
-//
-//            RestResponse restResponse = new RestResponse(response);
-//            RestResponse restResponse2 = new RestResponse(response);
-//            assert (restResponse.equals(restResponse2));
-//
-//            WebLinkingResponse webLinkingResponse = new WebLinkingResponse(response);
-//            WebLinkingResponse webLinkingResponse2 = new WebLinkingResponse(response);
-//            assert (webLinkingResponse.equals(webLinkingResponse2));
-//
-//            XmlResponse xmlResponse = new XmlResponse(response);
-//            XmlResponse xmlResponse2 = new XmlResponse(response);
-//            assert (xmlResponse.equals(xmlResponse2));
-//
-//            // Wire Classes
-//
-//            AutoRedirectingWire autoRedirectingWire = new AutoRedirectingWire(wire);
-//            AutoRedirectingWire autoRedirectingWire2 = new AutoRedirectingWire(wire);
-//            assert (autoRedirectingWire.equals(autoRedirectingWire2));
-//
-//            BasicAuthWire basicAuthWire = new BasicAuthWire(wire);
-//            BasicAuthWire basicAuthWire2 = new BasicAuthWire(wire);
-//            assert (basicAuthWire.equals(basicAuthWire2));
-//
-//            CachingWire cachingWire = new CachingWire(wire);
-//            CachingWire cachingWire2 = new CachingWire(wire);
-//            assert (cachingWire.equals(cachingWire2));
-//
-//            CookieOptimizingWire cookieOptimizingWire = new CookieOptimizingWire(wire);
-//            CookieOptimizingWire cookieOptimizingWire2 = new CookieOptimizingWire(wire);
-//            assert (cookieOptimizingWire.equals(cookieOptimizingWire2));
-//
-//            FcWire fcWire = new FcWire(wire);
-//            FcWire fcWire2 = new FcWire(wire);
-//            assert (fcWire.equals(fcWire2));
-//
-//            OneMinuteWire oneMinuteWire = new OneMinuteWire(wire);
-//            OneMinuteWire oneMinuteWire2 = new OneMinuteWire(wire);
-//            assert (oneMinuteWire.equals(oneMinuteWire2));
-//
-//            RetryWire retryWire = new RetryWire(wire);
-//            RetryWire retryWire2 = new RetryWire(wire);
-//            assert (retryWire.equals(retryWire2));
-//
-//            TrustedWire trustedWire = new TrustedWire(wire);
-//            TrustedWire trustedWire2 = new TrustedWire(wire);
-//            assert (trustedWire.equals(trustedWire2));
-//
-//            UserAgentWire userAgentWire = new UserAgentWire(wire);
-//            UserAgentWire userAgentWire2 = new UserAgentWire(wire);
-//            assert (userAgentWire.equals(userAgentWire2));
-//
-//            VerboseWire verboseWire = new VerboseWire(wire);
-//            VerboseWire verboseWire2 = new VerboseWire(wire);
-//            assert (verboseWire.equals(verboseWire2));
-//
-//        } catch (Exception e) {
-//            System.out.println("Exception in equalsTest: " + e.getMessage());
-//        }
-//    }
-//
-//    // BaseRequest reset
-//}
+package com.jcabi.http;
+
+import com.jcabi.http.request.*;
+import com.jcabi.http.response.*;
+import com.jcabi.http.wire.*;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import com.jcabi.immutable.Array;
+import java.util.AbstractMap;
+import java.net.URL;
+import java.util.Map;
+
+public class CustomRegressionTests {
+
+    // TRIVIAL TESTS: ---------------------------------------
+    // This tests the toString, equals, and hashCode methods of all classes.
+    // These are often generated through the lombok package and aren't really related to functionality.
+    // Certain other trivial coverage of, for example, unused constructors is also achieved here.
+    @Test
+    void toStringTests() throws Exception {
+        try {
+            // Requests Classes
+            String urlString = "http://localhost:8080/";
+            URL basicURL = new URL(urlString);
+            Wire wire = Mockito.mock(Wire.class);
+
+            ApacheRequest apacheRequest = new ApacheRequest(basicURL);
+            String expected = "HTTP/1.1 GET / (localhost)\n\n";
+            assert(apacheRequest.toString().contains(expected));
+
+            BaseRequest baseRequest = new BaseRequest(wire, urlString);
+            assert(baseRequest.toString().contains(expected));
+            assert(baseRequest.uri().toString().equals(urlString));
+            RequestBody body = baseRequest.body().formParam("a","b").formParam("c","d");
+            assert(body.toString().equals("a=b&c=d"));
+            assert(baseRequest.multipartBody().toString().contains("<<empty>>"));
+
+            FakeRequest fakeRequest = new FakeRequest();
+            assert(fakeRequest.toString().contains("FakeRequest"));
+
+            JdkRequest jdkRequest = new JdkRequest(urlString);
+            assert(jdkRequest.toString().contains("JdkRequest"));
+
+            // Wire Classes
+            AutoRedirectingWire wire1 = new AutoRedirectingWire(wire);
+            assert(wire1.toString().contains("AutoRedirectingWire"));
+
+            BasicAuthWire wire2 = new BasicAuthWire(wire);
+            assert(wire2.toString().contains("BasicAuthWire"));
+
+            CachingWire wire3 = new CachingWire(wire);
+            assert(wire3.toString().contains("CachingWire"));
+
+            CookieOptimizingWire wire4 = new CookieOptimizingWire(wire);
+            assert(wire4.toString().contains("CookieOptimizingWire"));
+
+            ETagCachingWire wire5 = new ETagCachingWire(wire);
+            assert(wire5.toString().contains("ETagCachingWire"));
+
+            FcWire wire6 = new FcWire(wire);
+            assert(wire6.toString().contains("FcWire"));
+
+            LastModifiedCachingWire wire7 = new LastModifiedCachingWire(wire);
+            assert(wire7.toString().contains("LastModifiedCachingWire"));
+
+            OneMinuteWire wire8 = new OneMinuteWire(wire);
+            assert(wire8.toString().contains("OneMinuteWire"));
+
+            RetryWire wire9 = new RetryWire(wire);
+            assert(wire9.toString().contains("RetryWire"));
+
+            TrustedWire wire10 = new TrustedWire(wire);
+            assert(wire10.toString().contains("TrustedWire"));
+
+            UserAgentWire wire11 = new UserAgentWire(wire);
+            assert(wire11.toString().contains("UserAgentWire"));
+
+            VerboseWire wire12 = new VerboseWire(wire);
+            assert(wire12.toString().contains("VerboseWire"));
+
+        } catch (Exception e) {
+            System.out.println("Exception in toStringTests: " + e.getMessage());
+        }
+    }
+
+    @Test
+    void equalsTest() throws Exception {
+        try {
+            String urlString = "http://localhost:8080/";
+            URL basicURL = new URL(urlString);
+            Wire wire = Mockito.mock(Wire.class);
+
+            // Create identical headers and content
+            Array<Map.Entry<String, String>> headers = new Array<>(
+                    new AbstractMap.SimpleEntry<>("Content-Type", "application/json"),
+                    new AbstractMap.SimpleEntry<>("Authorization", "Bearer token")
+            );
+            byte[] content = "response body".getBytes();
+
+            ApacheRequest apacheRequest = new ApacheRequest(basicURL);
+            ApacheRequest apacheRequest2 = new ApacheRequest(basicURL);
+            assert (apacheRequest.equals(apacheRequest2));
+
+            DefaultResponse defaultResponse = new DefaultResponse(apacheRequest, 200, "OK", headers, content);
+            DefaultResponse defaultResponse2 = new DefaultResponse(apacheRequest2, 200, "OK", headers, content);
+            assert (defaultResponse.equals(defaultResponse2));
+
+            FakeRequest fakeRequest = new FakeRequest();
+            FakeRequest fakeRequest2 = new FakeRequest();
+            assert (fakeRequest.equals(fakeRequest2));
+
+            JdkRequest jdkRequest = new JdkRequest(urlString);
+            JdkRequest jdkRequest2 = new JdkRequest(urlString);
+            assert (jdkRequest.equals(jdkRequest2));
+
+            // Response Classes
+            Response response = Mockito.mock(Response.class);
+
+            JacksonResponse jacksonResponse = new JacksonResponse(response);
+            JacksonResponse jacksonResponse2 = new JacksonResponse(response);
+            assert (jacksonResponse.equals(jacksonResponse2));
+
+            JsonResponse jsonResponse = new JsonResponse(response);
+            JsonResponse jsonResponse2 = new JsonResponse(response);
+            assert (jsonResponse.equals(jsonResponse2));
+
+            JsoupResponse jsoupResponse = new JsoupResponse(response);
+            JsoupResponse jsoupResponse2 = new JsoupResponse(response);
+            assert (jsoupResponse.equals(jsoupResponse2));
+
+            RestResponse restResponse = new RestResponse(response);
+            RestResponse restResponse2 = new RestResponse(response);
+            assert (restResponse.equals(restResponse2));
+
+            WebLinkingResponse webLinkingResponse = new WebLinkingResponse(response);
+            WebLinkingResponse webLinkingResponse2 = new WebLinkingResponse(response);
+            assert (webLinkingResponse.equals(webLinkingResponse2));
+
+            XmlResponse xmlResponse = new XmlResponse(response);
+            XmlResponse xmlResponse2 = new XmlResponse(response);
+            assert (xmlResponse.equals(xmlResponse2));
+
+            // Wire Classes
+
+            AutoRedirectingWire autoRedirectingWire = new AutoRedirectingWire(wire);
+            AutoRedirectingWire autoRedirectingWire2 = new AutoRedirectingWire(wire);
+            assert (autoRedirectingWire.equals(autoRedirectingWire2));
+
+            BasicAuthWire basicAuthWire = new BasicAuthWire(wire);
+            BasicAuthWire basicAuthWire2 = new BasicAuthWire(wire);
+            assert (basicAuthWire.equals(basicAuthWire2));
+
+            CachingWire cachingWire = new CachingWire(wire);
+            CachingWire cachingWire2 = new CachingWire(wire);
+            assert (cachingWire.equals(cachingWire2));
+
+            CookieOptimizingWire cookieOptimizingWire = new CookieOptimizingWire(wire);
+            CookieOptimizingWire cookieOptimizingWire2 = new CookieOptimizingWire(wire);
+            assert (cookieOptimizingWire.equals(cookieOptimizingWire2));
+
+            FcWire fcWire = new FcWire(wire);
+            FcWire fcWire2 = new FcWire(wire);
+            assert (fcWire.equals(fcWire2));
+
+            OneMinuteWire oneMinuteWire = new OneMinuteWire(wire);
+            OneMinuteWire oneMinuteWire2 = new OneMinuteWire(wire);
+            assert (oneMinuteWire.equals(oneMinuteWire2));
+
+            RetryWire retryWire = new RetryWire(wire);
+            RetryWire retryWire2 = new RetryWire(wire);
+            assert (retryWire.equals(retryWire2));
+
+            TrustedWire trustedWire = new TrustedWire(wire);
+            TrustedWire trustedWire2 = new TrustedWire(wire);
+            assert (trustedWire.equals(trustedWire2));
+
+            UserAgentWire userAgentWire = new UserAgentWire(wire);
+            UserAgentWire userAgentWire2 = new UserAgentWire(wire);
+            assert (userAgentWire.equals(userAgentWire2));
+
+            VerboseWire verboseWire = new VerboseWire(wire);
+            VerboseWire verboseWire2 = new VerboseWire(wire);
+            assert (verboseWire.equals(verboseWire2));
+
+        } catch (Exception e) {
+            System.out.println("Exception in equalsTest: " + e.getMessage());
+        }
+    }
+
+    // BaseRequest reset
+}
---
+++ src/test/java/com/jcabi/http/MethodCoverageTests.java
@@ -0,0 +1,245 @@
+package com.jcabi.http;
+
+import com.jcabi.http.request.*;
+import com.jcabi.http.response.*;
+import com.jcabi.http.wire.*;
+import com.jcabi.http.mock.MkAnswer;
+import com.jcabi.http.mock.MkContainer;
+import com.jcabi.http.mock.MkGrizzlyContainer;
+import com.jcabi.immutable.Array;
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.testcontainers.shaded.org.hamcrest.Matcher;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.util.AbstractMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Method coverage tests targeting specific API entry points.
+ */
+class MethodCoverageTests {
+
+    private MkContainer server;
+
+    @BeforeEach
+    void startServer() throws Exception {
+        server = new MkGrizzlyContainer()
+                .next(new MkAnswer.Simple(200, "{\"msg\":\"ok\"}")
+                        .withHeader("Content-Type", "application/json")
+                        .withHeader("Link", "<http://example.com/next>; rel=\"next\""))
+                .next(new MkAnswer.Simple(200, "{\"msg\":\"fresh\"}"))
+                .start();
+    }
+
+    @AfterEach
+    void stopServer() {
+        if (server != null) {
+            server.stop();
+        }
+    }
+
+    // ApacheRequest constructors, reset, body, multipartBody, fetch, through
+
+    @Test
+    void apacheRequestCtorAndReset() throws IOException {
+        ApacheRequest req1 = new ApacheRequest(server.home());
+        InputStream in = new ByteArrayInputStream("data".getBytes());
+        Response res1 = req1.fetch(in);
+        // reset on a fresh request
+        Request req2 = req1.reset("Any-Header");
+        Response res2 = req2.fetch();
+        assertEquals(200, res2.status());
+    }
+
+    @Test
+    void bodyAndMultipartBodyApis() {
+        ApacheRequest req = new ApacheRequest(server.home());
+        // Form-encoded body
+        RequestBody form = req.body();
+        form = form.formParams(Map.of("foo", "bar")); // FormEncodedBody.formParams
+        assertTrue(form.get().contains("foo=bar"));
+        // Multipart form body
+        RequestBody multi = req.multipartBody();
+        multi = multi.set("text");               // set(String)
+        multi = multi.set("bin".getBytes());    // set(byte[])
+        String get = multi.get(); // not used
+        JsonObject json = Json.createObjectBuilder().add("x", 1).build();
+        multi = multi.set(json);                  // set(JsonStructure)
+        multi = multi.formParams(Map.of("a", "b")); // MultipartFormBody.formParams
+        assertTrue(multi.get().contains("a"));      // get()
+    }
+
+    @Test
+    void fetchInputStreamAndThroughWire() throws Exception {
+        URI home = server.home();
+        ApacheRequest req = new ApacheRequest(home);
+        InputStream in = new ByteArrayInputStream("data".getBytes());
+        // through(Class<T>) overload
+        Request viaClass = req.through(CachingWire.class);
+        assertEquals(200, viaClass.fetch(in).status()); // fetch(InputStream)
+        // through(Wire) overload
+        Wire custom = Mockito.mock(Wire.class);
+        Request viaObj = req.through(custom);
+        assertEquals(200, viaObj.fetch().status());
+    }
+
+    // JsonResponse.VerboseReader.close()
+
+    @Test
+    void jsonResponseVerboseReaderClose() throws Exception {
+        URI home = server.home();
+        JsonResponse jr = new ApacheRequest(home)
+                .fetch()
+                .as(JsonResponse.class);
+        jr.json().close();
+    }
+
+    // WebLinkingResponse.SimpleLink Map methods
+
+    @Test
+    void simpleLinkMapMethods() throws Exception {
+        URI home = server.home();
+        WebLinkingResponse wlr = new ApacheRequest(home)
+                .fetch()
+                .as(WebLinkingResponse.class);
+        WebLinkingResponse.Link link = wlr.links().get("next");
+        assertNotNull(link);
+        // size/isEmpty
+        assertEquals(1, link.size());
+        assertFalse(link.isEmpty());
+        // containsKey/containsValue
+        assertTrue(link.containsKey("rel"));
+        assertTrue(link.containsValue("next"));
+        // keySet/values
+        assertTrue(link.keySet().contains("rel"));
+        assertTrue(link.values().contains("next"));
+        // mutation methods throw
+        assertThrows(UnsupportedOperationException.class, () -> link.put("a","b"));
+        assertThrows(UnsupportedOperationException.class, () -> link.remove("rel"));
+        assertThrows(UnsupportedOperationException.class, () -> link.putAll(Map.of("x","y")));
+        assertThrows(UnsupportedOperationException.class, link::clear);
+    }
+
+    // XmlResponse.rel and CachingWire.invalidate
+
+    @Test
+    void xmlResponseRelAndCachingInvalidate() throws Exception {
+        URI home = server.home();
+        // prepare XML link
+        String xml = "<root><link rel=\"self\" href=\"http://example.com/path\"/></root>";
+        server.next(new MkAnswer.Simple(200, xml)
+                .withHeader("Content-Type", "text/xml"));
+        Request next = new ApacheRequest(home)
+                .fetch()
+                .as(XmlResponse.class)
+                .rel("/root/link/@href");
+        assertNotNull(next);
+        // caching invalidate
+        Request cached = new ApacheRequest(home).through(CachingWire.class);
+        cached.fetch();
+        assertEquals(1, server.queries());
+        CachingWire.invalidate();
+        cached.fetch();
+        assertEquals(2, server.queries());
+    }
+
+    // toString and equals/hashCode for BaseUri and FormEncodedBody
+
+    @Test
+    void toStringAndEqualsHashCodeTests() throws IOException {
+        URI home = server.home();
+        ApacheRequest areq = new ApacheRequest(home);
+        // ApacheRequest.toString should include HTTP line
+        String atext = areq.toString();
+        assertTrue(atext.contains("HTTP/1.1 GET"));
+        // BaseRequest via ApacheRequest covers BaseRequest.toString
+        // BaseUri equality
+        RequestURI uri1 = areq.uri();
+        RequestURI uri2 = areq.uri();
+        assertEquals(uri1, uri2);
+        assertEquals(uri1.hashCode(), uri2.hashCode());
+        // FormEncodedBody equals/hashCode
+        RequestBody body1 = areq.body().formParams(Map.of("k","v"));
+        RequestBody body2 = areq.body().formParams(Map.of("k","v"));
+        assertEquals(body1, body2);
+        assertEquals(body1.hashCode(), body2.hashCode());
+        // WeblinkingRespones#SimpleLink hashCode
+        WebLinkingResponse wlr = new ApacheRequest(home)
+                .fetch()
+                .as(WebLinkingResponse.class);
+        WebLinkingResponse.Link link = wlr.links().get("next");
+        WebLinkingResponse.Link link2 = wlr.links().get("next");
+        assertEquals(link, link2);
+    }
+
+    @Test
+    void fakeRequestThroughClassWithArgsWorks() throws Exception {
+        FakeRequest fake = new FakeRequest().withStatus(201);
+        Request decorated = fake.through(RetryWire.class, 5);
+        Response res = decorated.fetch();
+        assertEquals(201, res.status());
+    }
+
+    @Test
+    void restAssert() throws IOException {
+        Response res = new ApacheRequest(server.home())
+                .fetch()
+                .as(RestResponse.class);
+        RestResponse rest = new RestResponse(res);
+        rest.assertThat(Matchers.equalTo(res));
+    }
+
+    @Test
+    void jsonAssert() {
+        try {
+            JsonResponse res = new JdkRequest(server.home())
+                    .fetch().as(JsonResponse.class);
+            JsonResponse r2 = res.assertJson("its unsupported anyways");
+        } catch (Exception e) {
+            assertTrue(e.getMessage().contains("unsupported"));
+        }
+    }
+
+    @Test
+    void abstractReason() {
+        try {
+            JsonResponse rp = new JdkRequest(server.home()).fetch().as(JsonResponse.class);
+            rp.reason();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    void fcWireEqualsAndHashCodeTest() {
+        Wire wire = Mockito.mock(Wire.class);
+        FcWire f1 = new FcWire(wire, "one", "two");
+        FcWire f2 = new FcWire(wire, "one", "two");
+        FcWire f3 = new FcWire(wire, "one", "three");
+        assertEquals(f1, f2);
+        assertEquals(f1.hashCode(), f2.hashCode());
+        assertNotEquals(f1, f3);
+    }
+
+    @Test
+    void oneMinuteWireSendTest() throws Exception {
+        URI home = server.home();
+        Request req = new ApacheRequest(home).through(OneMinuteWire.class);
+        assertEquals(200, req.fetch().status());
+    }
+
+
+
+}
